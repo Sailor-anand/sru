5a731b581ea97024565eebbf480f83353e45c615
Sailor-anand <147530396+Sailor-anand@users.noreply.github.com>
2024-12-09 22:30:46 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const exprees=require('express');
 const app=express();
 app.get('/',(req,res)=>{
-  res.get("hello");
+  res.send("hello");
 });
 app.listen(7000,()=>{
   console.log("runing at the 7000");
